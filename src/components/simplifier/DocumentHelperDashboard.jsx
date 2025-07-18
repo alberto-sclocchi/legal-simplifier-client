f4a2b8c576dfafd47bdf86048b03cff00d811b33
Alberto Sclocchi <scloks75tv@gmail.com>
2025-07-18 13:50:09 +0200

    finish document helper UI

--- src/components/simplifier/DocumentHelperDashboard.jsx
+++ src/components/simplifier/DocumentHelperDashboard.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import SimplifiedTextContainer from './SimplifiedTextContainer'
+import { FaCircleStop, FaMicrophone } from 'react-icons/fa6'
 
 export default function DocumentHelperDashboard() {
   return (
-    <div>DocumentHelperDashboard</div>
+    <div className='document-helper-dashboard'>
+        <SimplifiedTextContainer />
+        <form>
+            <input type='text' />
+            {/* <FaCircleStop />
+            <FaMicrophone /> */}
+            <button className='document-helper-btn'>submit</button>
+        </form>
+    </div>
   )
 }
